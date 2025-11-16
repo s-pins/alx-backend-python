097034759973c5a85b336ac17429ec5582beab9e
frank <167516336+s-pins@users.noreply.github.com>
2025-11-16 23:54:52 +0300

    Add organization and repository payloads for tests

--- 0x03-Unittests_and_integration_tests/fixtures.py
+++ 0x03-Unittests_and_integration_tests/fixtures.py
@@ -948,3 +948,12 @@ TEST_PAYLOAD = [
     ['dagger', 'kratu', 'traceur-compiler', 'firmata.py'],
   )
 ]
+
+org_payload = {"login": "apache", "repos_url": "https://api.github.com/orgs/apache/repos"}
+repos_payload = [
+    {"name": "repo1", "license": {"key": "apache-2.0"}},
+    {"name": "repo2", "license": {"key": "other"}},
+    {"name": "repo3", "license": {"key": "apache-2.0"}},
+]
+expected_repos = ["repo1", "repo2", "repo3"]
+apache2_repos = ["repo1", "repo3"]
