bed883db718ab5e2f3da4de55ab17a4767a4fe73
frank <167516336+s-pins@users.noreply.github.com>
2025-11-18 09:25:22 +0300

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,7 +4,7 @@
 import unittest
 import requests
 from unittest.mock import patch, PropertyMock, Mock
-from parameterized import parameterized_class
+from parameterized import parameterized, parameterized_class
 
 from client import GithubOrgClient
 from fixtures import org_payload, repos_payload, expected_repos, apache2_repos
